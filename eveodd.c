f05d68e379a6b74e3be1dd7f1b93ff0c54e54e9f
kodiyarasu <35652395+kodiyarasu@users.noreply.github.com>
2018-01-21 15:22:38 +0530

    Create eveodd.c

---
+++ eveodd.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+int a;
+printf("Enter the number");
+scanf("%d",&a);
+if(a%2==0)
+{
+printf("\n The number is even");
+}
+else
+{
+printf("\n The number is odd");
+}
+}
