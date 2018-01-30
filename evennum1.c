57194e7b14ae3eb9ba8e57b5be684472f5869aed
kodiyarasu <35652395+kodiyarasu@users.noreply.github.com>
2018-01-30 17:01:25 +0530

    Rename evennum.c to evennum1.c

---
+++ evennum1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,i;
+printf("Enter the first region:");
+scanf("%d",&a);
+printf("Enter the final region:");
+scanf("%d",&b);
+for(i=a;i<=b;++);
+{
+if(a%2==0)
+{
+printf("\n Even number",a;
+}
+}
