77c46d58f838aca3c1c4e37d867b7f0e637d9fd1
kodiyarasu <35652395+kodiyarasu@users.noreply.github.com>
2018-02-22 20:22:11 +0530

    Create nearest power of 2.c

---
+++ nearest power of 2.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b.c.n.i;
+printf("Enter the number:");
+scanf("%d",&n);
+for(i=1;i<n;i++)
+{
+a=pow(2,i);
+{
+if(a>n)
+{
+b=i;
+break;
+}
+}
+}
+c=pow(2,b);
+printf("%d",c);
+}
