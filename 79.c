48d94d99c6753ef5f8614bd58f60fc8c5f295707
kodiyarasu <35652395+kodiyarasu@users.noreply.github.com>
2018-03-02 14:49:31 +0530

    Create 79.c

---
+++ 79.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,c,n;
+printf("Enter the values of a and b:");
+scanf("%d%d",&,&b);
+c=a*b;
+i=sqrt(c);
+if(c==i*i)
+{
+printf("YES");
+}
+else
+{
+printf("\n NO");
+}
+}
