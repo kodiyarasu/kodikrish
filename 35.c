937be107a7c7120b4b465ddfe10d0bc0d59a2baa
kodiyarasu <35652395+kodiyarasu@users.noreply.github.com>
2018-03-13 15:44:13 +0530

    Create 35.c

---
+++ 35.c
@@ -0,0 +1,19 @@
+
+#include <stdio.h>
+#include<conio.h>
+#include<string.h>
+void main() 
+{
+char a[1000];
+int b,i,count=0;
+scanf("%s",&a);
+b=strlen(a);
+for(i=0;i<n;i++)
+{
+if(a[i]>='0' && a[i]<='9')
+{
+count++;
+}
+}
+printf("%d",count);
+}
