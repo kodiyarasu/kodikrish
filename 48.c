4bf06e887af7e051aa11427c81130c18b7404ca9
kodiyarasu <35652395+kodiyarasu@users.noreply.github.com>
2018-03-13 16:29:20 +0530

    Create 48.c

---
+++ 48.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+void main() 
+{
+int n,sum=0,i,avg;
+int a[i];
+printf("Enter the value:");
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);	
+}
+for(i=0;i<n;i++)
+{
+sum=sum+a[i];
+}
+avg=sum/n;
+printf("%d",avg);
+}
